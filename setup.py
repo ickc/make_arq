1efec4aa1c0bc297e2e7a78bc01f929e76d5a8e3
Kolen Cheung <christian.kolen@gmail.com>
2021-02-07 03:33:58 -0800

    setup.py: drop pypy

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     install_requires=[
         "numpy",
