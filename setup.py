d77be178204c512c931200acd2dff27c30bdb359
Kolen Cheung <christian.kolen@gmail.com>
2021-02-07 01:36:31 -0800

    black

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup, Extension
 import numpy
 
-setup(name='_makearq', version='0.1',
-      description='make_arq helper',
-      ext_modules=[Extension('_makearq', ['_makearq.c'],
-                             include_dirs=[numpy.get_include()]
-                             )]
-      )
+setup(
+    name="_makearq",
+    version="0.1",
+    description="make_arq helper",
+    ext_modules=[
+        Extension("_makearq", ["_makearq.c"], include_dirs=[numpy.get_include()])
+    ],
+)
